75682bd05c31e66d0c832c7026567b7a05024606
Fortune Ikechi <ikechifortune@gmail.com>
2024-10-19 12:14:44 +0100

    updated the dockerfile
    Signed-off-by: Fortune Ikechi <ikechifortune@gmail.com>

--- web/db/plugins/greetings.ts
+++ web/db/plugins/greetings.ts
@@ -6,38 +6,13 @@ import {
   FastifyReply,
 } from "fastify";
 
-declare module "fastify" {
-  interface FastifyInstance {
-    sayHello: (name: string) => string;
-  }
-}
-
 export default async function (
   fastify: FastifyInstance,
   opts: FastifyPluginOptions
 ) {
-  fastify.decorate("sayHello", sayHello);
+  fastify.decorate("sayHello", "sayHello");
 
   function sayHello(name: string) {
-    return `${opts.greeting || "Hello"} ${name}`;
+    return `${opts.greeting} ${name}`;
   }
-
-  fastify.get(
-    "/hello",
-    {
-      schema: {
-        querystring: {
-          type: "object",
-          properties: {
-            name: { type: "string" },
-          },
-        },
-      },
-    },
-    async (request: FastifyRequest, reply: FastifyReply) => {
-      const { name = "Guest" } = request.query as { name?: string };
-      const greeting = fastify.sayHello(name);
-      return { result: `${greeting}, Welcome to Platformatic` };
-    }
-  );
 }
